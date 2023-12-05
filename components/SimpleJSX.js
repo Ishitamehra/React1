a3c3019ea73376fcbdf14070858264e06f98f530
Ishitamehra <135868668+Ishitamehra@users.noreply.github.com>
2023-12-05 18:01:02 +0530

    Add files via upload

---
+++ components/SimpleJSX.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SimpleJSX = () => {
+  return (
+    <div>
+        <h1>
+
+        </h1>
+      <h1>Hello, World!</h1>
+      <p>This is my first React task.</p>
+
+      <h3>I am displaying ARRAY OF RECORDS.</h3>
+    </div>
+  );
+};
+
+export default SimpleJSX;
